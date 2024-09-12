07170be2dfd083e0a5b1b5109c36cbc42dfa725c
Terminator15474 <jandommert8@gmail.com>
2024-09-12 18:49:20 +0200

    Refactored pixel scaling and window size

--- src/render.zig
+++ src/render.zig
@@ -2,8 +2,11 @@ const rl = @import("raylib");
 const std = @import("std");
 const png = @import("png.zig");
 
-pub fn renderPngInWindow(img_data: *png.png_data) !void {
-    rl.initWindow(@intCast(img_data.width * 3), @intCast(img_data.height * 3), "zig-png - example");
+pub fn renderPngInWindow(width: u32, height: u32, img_data: *png.png_data) !void {
+    const scaleX = width / img_data.width;
+    const scaleY = height / img_data.height;
+
+    rl.initWindow(@intCast(width), @intCast(height), "zig-png - example");
     defer rl.closeWindow();
     rl.setTargetFPS(60);
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
@@ -14,33 +17,18 @@ pub fn renderPngInWindow(img_data: *png.png_data) !void {
 
         for (0..img_data.height) |x| {
             for (0..img_data.width) |y| {
-                const pixel = img_data.getPixel(x, y);
-                if (pixel == 1) {
-                    rl.drawPixel(@intCast(x * 3), @intCast(y * 3), rl.Color.white);
-                    rl.drawPixel(@intCast(x * 3 + 1), @intCast(y * 3), rl.Color.white);
-                    rl.drawPixel(@intCast(x * 3 + 2), @intCast(y * 3), rl.Color.white);
-
-                    rl.drawPixel(@intCast(x * 3), @intCast(y * 3 + 1), rl.Color.white);
-                    rl.drawPixel(@intCast(x * 3 + 1), @intCast(y * 3 + 1), rl.Color.white);
-                    rl.drawPixel(@intCast(x * 3 + 2), @intCast(y * 3 + 1), rl.Color.white);
-
-                    rl.drawPixel(@intCast(x * 3), @intCast(y * 3 + 2), rl.Color.white);
-                    rl.drawPixel(@intCast(x * 3 + 1), @intCast(y * 3 + 2), rl.Color.white);
-                    rl.drawPixel(@intCast(x * 3 + 2), @intCast(y * 3 + 2), rl.Color.white);
-                } else {
-                    rl.drawPixel(@intCast(x * 3), @intCast(y * 3), rl.Color.black);
-                    rl.drawPixel(@intCast(x * 3 + 1), @intCast(y * 3), rl.Color.black);
-                    rl.drawPixel(@intCast(x * 3 + 2), @intCast(y * 3), rl.Color.black);
-
-                    rl.drawPixel(@intCast(x * 3), @intCast(y * 3 + 1), rl.Color.black);
-                    rl.drawPixel(@intCast(x * 3 + 1), @intCast(y * 3 + 1), rl.Color.black);
-                    rl.drawPixel(@intCast(x * 3 + 2), @intCast(y * 3 + 1), rl.Color.black);
-
-                    rl.drawPixel(@intCast(x * 3), @intCast(y * 3 + 2), rl.Color.black);
-                    rl.drawPixel(@intCast(x * 3 + 1), @intCast(y * 3 + 2), rl.Color.black);
-                    rl.drawPixel(@intCast(x * 3 + 2), @intCast(y * 3 + 2), rl.Color.black);
-                }
+                const pixel = (img_data.getPixel(x, y) / std.math.pow(u8, 2, img_data.bit_depth)) * 255;
+                std.log.debug("BD: {d}, val: {d}\n", .{ img_data.bit_depth, pixel });
+                drawPixelScaled(@intCast(x), @intCast(y), scaleX, scaleY, rl.Color.init(pixel, pixel, pixel, 255));
             }
+        }
+    }
+}
+
+fn drawPixelScaled(x: u32, y: u32, scaleX: u32, scaleY: u32, color: rl.Color) void {
+    for (0..scaleX) |i| {
+        for (0..scaleY) |j| {
+            rl.drawPixel(@intCast(x * scaleX + i), @intCast(y * scaleY + j), color);
         }
     }
 }
